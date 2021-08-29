127b179d302f2c525afd29a2ea4e1e84955a02ed
Simran Bhoneja <spbhoneja@gmail.com>
2021-08-29 12:57:17 +0530

    URL added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Category, Post
 
 # Create your views here.
+def post(request, slug): 
+    post = Post.objects.filter(slug = slug).first()
+    return HttpResponse(f"<h1> {post.title} </h1> <br> <p> {post.content}</p>")
+
+def index(request):
+    post = Post.objects.all()
+    print(post)
+    return HttpResponse("Done")
